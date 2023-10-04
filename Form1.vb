ec328a939712d47779d17d51e4feab883efcd127
Dell-User <Dell-User@DESKTOP-BSDRSH6>
2023-10-04 14:15:58 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim x, y, z, year
+
+        year = TextBox1.Text
+        x = year Mod 4
+        y = year Mod 100
+        z = year Mod 400
+
+        If ((x = 0 And Not (y = 0)) Or z = 0) Then
+            Label2.Text = "this is a leap year"
+        Else
+            Label2.Text = "this is not a leap year"
+        End If
+
+
+    End Sub
+End Class
